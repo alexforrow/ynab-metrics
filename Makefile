c35ed58d543c135df35f86a80314193bd0e644a0
Alex Forrow <alex@forrow.com>
2026-03-26 23:27:53 +0000

    experiment to count unapproved and uncategorised transactions

--- Makefile
+++ Makefile
@@ -6,5 +6,8 @@ CFGFILE=config.json
 build:
 	go build -o ${BINARY} -v
 
+arm:
+	GOOS=linux GOARCH=arm64 go build -o ${BINARY}.arm64 -v
+
 run:
 	./${BINARY} --config ${CFGFILE}
--- pkg/accounts/accounts.go
+++ pkg/accounts/accounts.go
@@ -1,12 +1,16 @@
 package accounts
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	u "github.com/hoenn/ynab-metrics/pkg/units"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/brunomvsouza/ynab.go"
 	"github.com/brunomvsouza/ynab.go/api/budget"
+	"github.com/brunomvsouza/ynab.go/api/transaction"
+	"github.com/brunomvsouza/ynab.go/api"
 )
 
 var accountBalance = prometheus.NewGaugeVec(prometheus.GaugeOpts{
@@ -15,17 +19,61 @@ var accountBalance = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 },
 	[]string{"budget_name", "name"})
 
+var attentionTransactions = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+	Name: "attention_transactions",
+	Help: "Transactions requiring attention gauge",
+},
+	[]string{"budget_name", "account_name", "status"})
+
 func init() {
 	prometheus.MustRegister(accountBalance)
+	prometheus.MustRegister(attentionTransactions)
 }
 
 //StartMetrics collects accounts metrics given a list of budgets
 func StartMetrics(c ynab.ClientServicer, budgets []*budget.Budget) {
 	log.Println("Getting Accounts...")
 
+	statuses := map[string]*transaction.Status{
+		"uncategorized": transaction.StatusUncategorized.Pointer(),
+		"unapproved": transaction.StatusUnapproved.Pointer(),
+	}
+
+	// limit transactions to X months ago
+	startDate := time.Now().AddDate(0, -1 ,0)
+	startDateStr := startDate.Format(time.DateOnly)
+	log.Print(fmt.Sprintf("Start date is: %s", startDateStr))
+
 	for _, b := range budgets {
 		for _, a := range b.Accounts {
+			// skip unused accounts
+			if a.Closed || a.Deleted {
+				log.Print(fmt.Sprintf("Skipping account %s/%s as its closed or deleted", b.Name, a.Name))
+				continue
+			}
+
+			// output balance
 			accountBalance.WithLabelValues(b.Name, a.Name).Set(float64(u.Dollars(a.Balance)))
+
+			// for each account count number of transactions needing attention (unapproved or uncategorised)
+			startDate, _ := api.DateFromString(startDateStr)
+
+			for status, statusPointer := range statuses {
+				filter := &transaction.Filter{
+					Since: &startDate,
+					Type:  statusPointer,
+				}
+
+				transactions, err := c.Transaction().GetTransactionsByAccount(
+					b.ID, a.ID, filter)
+
+				if err != nil {
+					log.Print(fmt.Sprintf("Unable to get %s transactions for budget/account: %s/%s (%s)", status, b.Name, a.Name, err))
+					continue
+				}
+
+				attentionTransactions.WithLabelValues(b.Name, a.Name, status).Set(float64(len(transactions)))
+			}
 		}
 	}
 }
